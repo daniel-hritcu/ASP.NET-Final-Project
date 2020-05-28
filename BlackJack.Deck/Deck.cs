c7c871d5955a600d200f9f4647a7c1f4db5c8746
Daniel Hritcu <daniel@hritcu.ca>
2020-05-28 17:28:44 -0400

    Implement StandardDeck

---
+++ BlackJack.Deck/Deck.cs
@@ -0,0 +1,96 @@
+﻿using BlackJack.StandardDeck.Exceptions;
+using BlackJack.Domain.Interfaces;
+using BlackJack.Domain.PlayingCard;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace BlackJack.StandardDeck
+{
+    /// <summary>
+    /// Implements the functionality of a standard deck of playing cards
+    /// </summary>
+    ///<seealso cref = "Domain.Interfaces.IDeck">
+    /// Method documentation at the interface.
+    ///</seealso>
+    public class Deck : IDeck
+    {
+        /// <summary>
+        /// Provides the Random used in the Shuffle method.
+        /// </summary>
+        private static readonly Random Random = new Random();
+
+        /// <summary>
+        /// Sets the number of times the deck is shuffled.
+        /// </summary>
+        private const int _shuffleTimes = 100;
+
+        public List<Card> Cards { get; set; }
+
+        public Deck()
+        {
+            Setup();
+        }
+
+        
+        public void Setup()
+        {
+            //Populate deck with cards
+            Cards = new List<Card>(
+                Enumerable.Range(1, 4)
+                .SelectMany(suit => Enumerable.Range(2, 13)
+                .Select(rank => new Card
+                {
+                    Suit = (CardSuit) suit,
+                    Rank = (CardRank) rank
+                })));
+
+            //Shuffle the new deck
+            Shuffle();
+        }
+
+        public void Shuffle()
+        {
+            int cardsCount = CardCount();
+
+            //Shuffle the ammount of times defined in _shuffleTimes.
+            for (int i = 0; i < _shuffleTimes; i++) {
+
+                while (cardsCount > 1)
+                {
+                    cardsCount--;
+
+                    int index = Random.Next(cardsCount + 1);
+                    Card card = Cards[index];
+                    Cards[index] = Cards[cardsCount];
+                    Cards[cardsCount] = card;
+                }
+
+            }
+        }
+
+       
+        public Card Draw()
+        {
+            if (CardCount() == 0)
+            {
+                throw new EmptyDeckException("Deck does not have any cards.");
+            } 
+
+            //Select card
+            Card drawnCard = Cards[0];
+
+            //Remove from deck
+            Cards.RemoveAt(0);
+
+            //Return selected card
+            return drawnCard;
+        }
+
+        public int CardCount()
+        {
+            return Cards.Count;
+        }
+    }
+}
---
+++ BlackJack.Deck/Exceptions/EmptyDeckException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.StandardDeck.Exceptions
+{
+    public class EmptyDeckException : Exception
+    {
+        public EmptyDeckException()
+        {
+        }
+
+        public EmptyDeckException(string message) : base(message)
+        {
+        }
+
+        public EmptyDeckException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
